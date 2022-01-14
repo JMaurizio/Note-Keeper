f35b8e81a8a66093aed15d577a8d1f3b285065da
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-01-14 15:03:48 -0600

    added get post and delete routes

---
+++ routes/api-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const note = require("../lib/note");
+const notes = require("../db/db.json");
+
+router.get("/notes", (req, res) => {
+    res.status(200).json(notes);
+});
+
+router.post('/notes', (req,res) => {
+    const newNote = note.createNote(req.body, notes);
+    res.status(200).json(newNote);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    const remove = note.deleteNote(req.params.id, notes)
+    res.status(200).json(remove)
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const apiRoute = require("./routes/api-routes");
+const htmlRoute = require("./routes/html-routes");
+
+const PORT = process.env.PORT || 4001;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoute);
+app.use("/", htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`API server is on port ${PORT}`);
+});
